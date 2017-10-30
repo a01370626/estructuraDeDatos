c9405ee9bf3450a91e091a6d50e16bf65a31a432
Yael Mendoza <yamb_123459@msn.com>
2017-10-30 10:10:23 -0600

    Actividad de CodingBat corregida, este si es el bueno

--- src/practica3/Main.java
+++ src/practica3/Main.java
@@ -10,6 +10,196 @@ package practica3;
  */
 public class Main {
     
+
+          
+
+          public int factorial(int n) {
+
+            if(n == 0){
+
+                return 1;
+
+            }else{
+
+                return n * factorial(n-1);
+
+            }
+
+          }
+
+          
+
+            public int bunnyEars(int bunnies) {
+
+             if(bunnies == 0){
+
+               return 0;
+
+             }else{
+
+               return 2 * (bunnies);
+
+             }
+
+           }
+
+
+
+        public int fibonacci(int n) {
+
+          if(n <= 1){
+
+            return n;
+
+          }else{
+
+            return fibonacci(n-1) + fibonacci(n-2);
+
+          }
+
+        }
+
+
+
+        public int bunnyEars2(int bunnies) {
+
+          if(bunnies == 0){
+
+            return 0;
+
+          }else{
+
+            if(bunnies %2 != 0){
+
+              return bunnyEars2(bunnies - 1) + 2;
+
+            }else{
+
+              return bunnyEars2(bunnies - 1) + 3;
+
+            }
+
+          }
+
+        }        
+    
+    
+     public int triangle(int rows) {
+          if(rows == 0){
+            return 0;
+          }else{
+            return triangle(rows - 1) + rows;
+          }
+        }       
+        
+        public int sumDigits(int n) {
+          if(n == 0){
+            return 0;
+          }else{
+            return sumDigits(n/10) + n%10;
+          }
+        }
+        
+        public int count7(int n) {
+          if(n == 0){
+            return 0;
+          }else if(n%10 == 7){
+              return count7(n/10) + 1;
+            }
+            else{
+              return count7(n/10);
+            }
+        }        
+        
+        public int count8(int n) {
+          if(n == 0){
+            return 0;
+          }else if(n%10 == 8){
+            if(n / 10 % 10 == 8){
+              return count8(n/10) + 2;
+            }else{
+              return count8(n/10) + 1;
+            }
+          }
+            else{
+              return count8(n/10);
+            }
+
+        }
+
+
+        public int powerN(int base, int n) {
+
+          if(n == 1){
+            return base;
+          }else{
+            return base * powerN(base, n -1);
+          }
+        }
+        
+        public int countX(String str) {
+          if(str.length() == 0){
+            return 0;
+          }else if(str.charAt(0) == 'x'){
+            return 1 + countX(str.substring(1));
+          }else{
+            return countX(str.substring(1));
+          }
+        }
+
+        public String changeXY(String str) {
+          if(str.length() == 0){
+            return str;
+          }else if(str.charAt(0) == 'x'){
+            return 'y' + changeXY(str.substring(1));
+          }else{
+
+            return str.charAt(0) + changeXY(str.substring(1));
+
+          }
+
+        }
+
+        
+
+        public boolean array6(int[] nums, int index) {
+
+          if(index == nums.length){
+
+            return false;
+
+          }else if(nums[index] == 6){
+
+            return true;
+
+          }else{
+
+            return array6(nums, index +1);
+
+          }
+
+        }
+
+
+
+        public int array11(int[] nums, int index) {
+
+          if(index == nums.length){
+
+            return 0;
+
+          }else if (nums[index] == 11){
+
+            return array11(nums, index + 1) + 1;
+
+          }else{
+
+            return array11(nums, index + 1);
+
+          }
+
+        }
+    
     /*public static boolean isPalindrome(String word){
         LinkedList<Character> letters = new LinkedList<Character>();
             for (int i = 0; i < word.length() ; i++){
@@ -28,40 +218,56 @@ public class Main {
         }   
             return true;
     }*/
+    
+    public static void heapSort( int values[]){
+        makeHeap(values);
+        
+        for (int i = values.length; i > 0; i--){
+            int lastItem = removeTopItem(values,i);
+            values[i-1] = lastItem;
+        }
+        
+    }
     public static String reverseParentheses(String cadena){
        Stack<Character> pila =new Stack<Character>();
         Character c=null;
         StringBuilder temp = new StringBuilder (cadena.length());
+        //StringBuilder temp2 = new StringBuilder (cadena.length());
         StringBuilder Inver =new StringBuilder (cadena.length());
         int bander = 0;
         
         for (int i=0; i<cadena.length(); i++){
             c=cadena.charAt(i);
-            if ((!(c.equals('('))) & (bander==0)){
+            if ((!(c.equals('('))) && (bander==0)){
                 Inver.append(c);
             }
             else if (c.equals('(')){
                  bander=bander+1;}
             
-           else if ((!(c.equals('('))) & (bander==1)){
+           else if ((!(c.equals('('))) && (bander==1)){
                     pila.push(c);
                 }
-           else if ((!(c.equals('('))) & (bander==2)){
-               temp.append(pila.pop());
-               Inver.append(temp.reverse());
+           else if ((!(c.equals('('))) && (bander==2)){
+               while (!(pila.empty())){
+                   temp.append(pila.pop());
+               }
+               Inver.append(temp);
                temp.delete(0,temp.length());
                bander=bander-1;
                 }
            else if ((c.equals(')'))){
-               temp.append(pila.pop());
-               Inver.append(temp.reverse());
+               while (!(pila.empty())){
+                   temp.append(pila.pop());
+               }
+               Inver.append(temp);
                temp.delete(0,temp.length());
                bander=bander-1;
                
            }
         }
     return Inver.toString();
-    }
+}
+  
     
 
     
@@ -183,7 +389,7 @@ public class Main {
         return result;
     }
     public static void main (String [] args){
-        String s = "a(bc)de";
+        String s = "afdfg(bc)de";
         System.out.println(reverseParentheses(s));
     }
     
